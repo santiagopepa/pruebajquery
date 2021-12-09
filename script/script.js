379096a4b694b6d3a12f18f83bc442a4aa36aee8
santiago <santiago_pepa93@hotmail.com>
2021-12-09 19:33:26 -0300

    nuevo commit

---
+++ script/script.js
@@ -0,0 +1,13 @@
+
+$(function(){
+
+
+    $("#tercero").mouseenter(function(){
+        $("#tercero").animate({width:"300px"})
+            $("#tercero").dblclick(function(){
+                $("#tercero").animate({width:"100%"})
+        })
+    })
+
+
+});
